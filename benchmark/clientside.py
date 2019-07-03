32a934d7ee0e5b22ce5fa87a74bd06bd301efa36
xifeiwang <xifeiw@uw.edu>
2019-07-04 01:41:02 +0800

    delete useless files

--- benchmark/clientside.py
+++
@@ -1,9 +0,0 @@
-import socket
-import cv2
-from _datetime import  datetime
-s = socket.socket()
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
-s.connect(('10.19.154.82', 5000))
-print(datetime.now())
-s.send(cv2.imencode('.jpg', frame)[1])
